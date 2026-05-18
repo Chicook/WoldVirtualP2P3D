44efd22f339d419f0daf60ef2e65998033c8a2db
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-05-19 01:24:07 +0200

    chore(visor): update build artifacts and add debug logs
    sync assembly informational version and sourcelink config to current repo commit
    add MainWindow startup debug log entries
    update all debug build output files

--- WoldVirtual/WoldVirtual3Dp2p/Capa3_Visor/obj/Debug/net8.0-windows/VisorSingularity.AssemblyInfo.cs
+++ WoldVirtual/WoldVirtual3Dp2p/Capa3_Visor/obj/Debug/net8.0-windows/VisorSingularity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28cf2bc13da82eaf76435628eecb91468fa79cb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+664fdd861e100f65f6b796da8d7e4b9d8180d4c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
