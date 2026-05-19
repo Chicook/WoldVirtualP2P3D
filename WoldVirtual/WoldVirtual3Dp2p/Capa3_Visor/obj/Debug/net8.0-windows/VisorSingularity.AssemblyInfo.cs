5321e9713e94ec817771c55b71f538ef09148bc7
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-05-19 05:03:01 +0200

    feat: implement SQLite DatabaseManager for user authentication and persistence

--- WoldVirtual/WoldVirtual3Dp2p/Capa3_Visor/obj/Debug/net8.0-windows/VisorSingularity.AssemblyInfo.cs
+++ WoldVirtual/WoldVirtual3Dp2p/Capa3_Visor/obj/Debug/net8.0-windows/VisorSingularity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d9eae745a4c31bb7e326424eedfdc89c6eba439")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+287f73839bedc15b85c20d9e7dbd447fa002210b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
