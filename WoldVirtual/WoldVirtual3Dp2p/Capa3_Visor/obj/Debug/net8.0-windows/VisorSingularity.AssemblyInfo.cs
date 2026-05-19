10da1508e3d06cff1a7a2a21495fa61f3451a08c
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-05-19 03:15:14 +0200

    build: initialize VisorSingularity project with .NET 8.0 Windows configuration

--- WoldVirtual/WoldVirtual3Dp2p/Capa3_Visor/obj/Debug/net8.0-windows/VisorSingularity.AssemblyInfo.cs
+++ WoldVirtual/WoldVirtual3Dp2p/Capa3_Visor/obj/Debug/net8.0-windows/VisorSingularity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee1c82422afa2f8f9b5ccc3d66508d3f0ab2e44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d3ce53b2b969cfaa6a915d7e1ea6b29385e082")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
