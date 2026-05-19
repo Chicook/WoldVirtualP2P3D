4fc996a8244e7102b8060b90e14976480666a031
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-05-19 02:11:01 +0200

    chore(visor): clean up debug SQLite files
    remove temporary SQLite write-ahead log and shared memory files, and update the main debug database

--- WoldVirtual/WoldVirtual3Dp2p/Capa3_Visor/obj/Debug/net8.0-windows/VisorSingularity.AssemblyInfo.cs
+++ WoldVirtual/WoldVirtual3Dp2p/Capa3_Visor/obj/Debug/net8.0-windows/VisorSingularity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34e3fa9491dd4056beaf7babe0065dcb183474d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8421c86d2a8521bda1964830a7a7ccd3fe65a9dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
