6d93306c682593e22c8c3f423e25621c14ce843f
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-05-21 04:31:33 +0200

    add

--- WoldVirtual/woldvirtual/ecs/components/IslandComponent.gd
+++
@@ -1,8 +0,0 @@
-extends ECSComponent
-class_name IslandComponent
-
-var id: String = ""
-var island_name: String = ""
-var owner_id: String = ""
-var is_active: bool = true
-var grid_pos: Vector2 = Vector2.ZERO
