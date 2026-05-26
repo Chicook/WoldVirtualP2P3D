7bb812f9d83d9e6a869b381579bffb0155f7610b
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-05-26 18:56:16 +0200

    add

--- Capa3_Visor/CapaVisor3D/obj/Debug/net8.0-windows/VisorSingularity_wxqxft2n_wpftmp.AssemblyInfo.cs
+++ Capa3_Visor/CapaVisor3D/obj/Debug/net8.0-windows/VisorSingularity_wxqxft2n_wpftmp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisorSingularity")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f81ef68789203b39b20035a3f344a79567e42a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisorSingularity")]
