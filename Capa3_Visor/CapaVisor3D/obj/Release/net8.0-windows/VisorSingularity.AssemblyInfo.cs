4f7f4af42590ad00d2033ebdbad60280d4718037
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-05-27 01:37:34 +0200

    add

--- Capa3_Visor/CapaVisor3D/obj/Release/net8.0-windows/VisorSingularity.AssemblyInfo.cs
+++ Capa3_Visor/CapaVisor3D/obj/Release/net8.0-windows/VisorSingularity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+816100b4b2039c1bcb7fe61bca8764110b75b8e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f7bdfdf450dc86f179b56226c25d21be57f790")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
