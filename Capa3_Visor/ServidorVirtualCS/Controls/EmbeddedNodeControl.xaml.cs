d867ab2381245ebcdf942a4d2adfc7d86d6aa4fc
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-05-27 00:10:15 +0200

    add

---
+++ Capa3_Visor/ServidorVirtualCS/Controls/EmbeddedNodeControl.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ServidorVirtualCS.Controls;
+
+public partial class EmbeddedNodeControl : UserControl
+{
+    public EmbeddedNodeControl()
+    {
+        InitializeComponent();
+    }
+}
