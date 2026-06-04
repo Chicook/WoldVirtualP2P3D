d9ea9e55317b0efa5e556a98b4655217ea8a9204
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-06-04 19:34:12 +0200

    add

--- Capa3_Visor/ServidorVirtualCS/obj/Debug/net8.0-windows/ServidorVirtualCS.AssemblyInfo.cs
+++ Capa3_Visor/ServidorVirtualCS/obj/Debug/net8.0-windows/ServidorVirtualCS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServidorVirtualCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5753ade0643ebe11ecf715e831548bf3f9f22b02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+211fe52087c232a760d6963a3b7b0ff7ee060f63")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServidorVirtualCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServidorVirtualCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Capa3_Visor/ServidorVirtualCS/obj/Release/net8.0-windows/ServidorVirtualCS_yzaezi4z_wpftmp.AssemblyInfo.cs
+++ Capa3_Visor/ServidorVirtualCS/obj/Release/net8.0-windows/ServidorVirtualCS_yzaezi4z_wpftmp.AssemblyInfo.cs
@@ -13,15 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServidorVirtualCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8209aff8c6284e10dc361616069db6df41717c3")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb927b6025ffb0246ab387c1b3b8d102ba33bd58")]
->>>>>>> main
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8209aff8c6284e10dc361616069db6df41717c3")]
->>>>>>> 06de4cb61f7851114abea818cec164ce2e9bd6f5
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+211fe52087c232a760d6963a3b7b0ff7ee060f63")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServidorVirtualCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServidorVirtualCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
