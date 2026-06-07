e326372c999e41e0862bdef39a033850993ac2ee
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-06-07 15:41:08 +0200

    add

--- Capa3_Visor/CapaVisor3D/obj/Release/net8.0-windows/VisorSingularity.AssemblyInfo.cs
+++ Capa3_Visor/CapaVisor3D/obj/Release/net8.0-windows/VisorSingularity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e20b10c61b7013c73b6b9f3b1bbdd4ecaee118")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327318ef9f9cd3e0c7fe1472b31a219a12f16bf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisorSingularity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Capa3_Visor/ServidorVirtualCS/obj/Release/net8.0-windows/ServidorVirtualCS_yzaezi4z_wpftmp.AssemblyInfo.cs
+++ Capa3_Visor/ServidorVirtualCS/obj/Release/net8.0-windows/ServidorVirtualCS_yzaezi4z_wpftmp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServidorVirtualCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3da6045c99a0bec262c99128fd77153889c17d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327318ef9f9cd3e0c7fe1472b31a219a12f16bf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServidorVirtualCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServidorVirtualCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
