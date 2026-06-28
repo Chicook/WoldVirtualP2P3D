189c82221bf09f99b10ff308348da3dad3947c3f
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2026-06-28 14:32:23 +0200

    add

--- Capa3_Visor/CapaVisor3D/Services/HardwareFingerprintService.cs
+++ Capa3_Visor/CapaVisor3D/Services/HardwareFingerprintService.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Management;
 using System.Security.Cryptography;
 using System.Text;
@@ -88,59 +87,6 @@ namespace VisorSingularity.Services
             }
 
             return builder.ToString();
-        }
-
-        public static string EncryptString(string plainText, string keyString)
-        {
-            if (string.IsNullOrEmpty(plainText)) return "";
-            try
-            {
-                byte[] key = SHA256.HashData(Encoding.UTF8.GetBytes(keyString));
-                byte[] iv = new byte[16];
-                Array.Copy(key, iv, 16);
-
-                using var aes = Aes.Create();
-                aes.Key = key;
-                aes.IV = iv;
-
-                using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-                using var ms = new MemoryStream();
-                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-                using (var sw = new StreamWriter(cs, Encoding.UTF8))
-                {
-                    sw.Write(plainText);
-                }
-                return Convert.ToBase64String(ms.ToArray());
-            }
-            catch
-            {
-                return plainText;
-            }
-        }
-
-        public static string DecryptString(string cipherText, string keyString)
-        {
-            if (string.IsNullOrEmpty(cipherText)) return "";
-            try
-            {
-                byte[] key = SHA256.HashData(Encoding.UTF8.GetBytes(keyString));
-                byte[] iv = new byte[16];
-                Array.Copy(key, iv, 16);
-
-                using var aes = Aes.Create();
-                aes.Key = key;
-                aes.IV = iv;
-
-                using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-                using var ms = new MemoryStream(Convert.FromBase64String(cipherText));
-                using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
-                using var sr = new StreamReader(cs, Encoding.UTF8);
-                return sr.ReadToEnd();
-            }
-            catch
-            {
-                return cipherText;
-            }
         }
     }
 }
